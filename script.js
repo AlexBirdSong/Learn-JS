d34cc1a28595f80aa26426de6faf56c49e149b30
AlexBirdSong <alexwhitechoco@gmail.com>
2020-07-05 20:57:02 +0300

    Вывод только простых чисел до 10

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 'use strict'
 
-for(let i = 0 ; i<=10 ; i++){
-    if(i%2==0){
-        alert(i);
-    }
+let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) { // Для всех i...
+
+  for (let j = 2; j < i; j++) { // проверить, делится ли число..
+    if (i % j == 0) continue nextPrime; // не подходит, берём следующее
+  }
+
+  alert( i ); // простое число
 }

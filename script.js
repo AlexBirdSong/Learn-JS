bd1321ce0a31863de91a8b56e9d59a4a23362ba1
AlexBirdSong <alexwhitechoco@gmail.com>
2020-07-07 20:40:53 +0300

    Стрелочная функция

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 'use strict'
 
-function findmax(a , b){
-  if (a>b){
-    return a;
-  }
-  else return b;
-};
-let i;
-i=findmax(2, 5);
-alert( i );
-i=findmax(3, -1);
-alert( i );
-i=findmax(1, 1);
-alert( i );
+ let ask = (question, yes, no) => {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  function() { alert("Вы согласились."); },
+  function() { alert("Вы отменили выполнение."); }
+);

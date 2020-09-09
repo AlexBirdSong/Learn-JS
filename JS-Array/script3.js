bb575302f40e4b0991d57aa44623698d8b91ad41
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-09 17:48:36 +0300

    add exercise solution 'Highest sum subarray'

--- JS-Array/script3.js
+++ JS-Array/script3.js
@@ -1,10 +1,21 @@
 'use strict'
-function truncate(str, maxlength){
-if (str.length>maxlength){
-let lowstr = str.slice(0,maxlength-1)+"...";
-alert(lowstr);
+
+function getMaxSubSum(arr){
+    let max = 0;
+   
+for(let j=0 ; j<arr.length; j++){
+    let sum = 0;
+    for(let i=j ; i<arr.length; i++){
+        sum +=  arr[i];
+        max = Math.max(max, sum);
+    } 
 }
-else alert(str);
+alert(max);
 }
 
-truncate("Вот, что мне хотелось бы сказать на эту тему:", 20)
+
+getMaxSubSum([-1, 2, 3, -9]); // 5
+getMaxSubSum([-1, 2, 3, -9, 11]); // 11
+getMaxSubSum([-2, -1, 1, 2]); // 3
+getMaxSubSum([1, 2, 3]); // 6
+getMaxSubSum([100, -9, 2, -3, 5]); // 100

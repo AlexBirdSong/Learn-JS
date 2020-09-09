9013df90e7bda3e3047ff7ecd63f37d32ab36275
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-09 16:47:27 +0300

    add exercise solution 'Sum of entered numbers'(arr)

--- JS-Array/script2.js
+++ JS-Array/script2.js
@@ -1,9 +1,18 @@
 'use strict'
 
-function checkSpam(str){
-    let lowerstr = str.toLowerCase();
-    if (lowerstr.includes('viagra')||(lowerstr.includes('xxx')))return true
-    else return false
+function sumInput(arr=[]){
+    let i=0;
+    do{
+        arr [i] = Number([prompt('Введите число',0)]);
+        i++;
+    }
+    while (Number.isInteger(arr[arr.length-1]));
+   let sum = 0;
+   for (let i = 0; i < arr.length-1; i++){
+    sum += arr[i];
+   }
+   alert(sum);
 }
-let str = prompt("Введите текст");
-alert(checkSpam(str));
+
+let numberArr = [];
+sumInput(numberArr);

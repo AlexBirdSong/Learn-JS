f9b0fdcf33ed816a48a382100a19d315ec6e3d16
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-11 23:17:02 +0300

    add exercise solution 'Show day of weak'

--- JS-Date/script2.js
+++ JS-Date/script2.js
@@ -1,2 +1,21 @@
 'use strict'
 
+function getWeekDay(date){
+let numDayWeak = date.getDay();
+let stringDayWeak;
+
+switch (numDayWeak){
+    case 0: stringDayWeak = "СБ";break;
+    case 1: stringDayWeak = "ВСК";break;
+    case 2: stringDayWeak = "ПН";break;
+    case 3: stringDayWeak = "ВТ";break;
+    case 4: stringDayWeak = "СР";break;
+    case 5: stringDayWeak = "ЧТ";break;
+    case 6: stringDayWeak = "ПТ";break;
+    default : stringDayWeak = "Некорректная дата"
+}
+alert(stringDayWeak);
+}
+
+let date = new Date();
+getWeekDay(date);

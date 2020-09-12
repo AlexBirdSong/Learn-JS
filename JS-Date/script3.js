2561f7f0df0d01067bbb4e0a30160056e4983773
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-12 14:56:55 +0300

    add exercise solution 'What day of the month was many days ago?'

--- JS-Date/script3.js
+++ JS-Date/script3.js
@@ -1,2 +1,18 @@
 'use strict'
 
+function getDateAgo(date, days){
+    let day = date.getDate();
+    let mounth = date.getMonth();
+    let year = date.getFullYear();
+    let bufDay = day - days;
+    // alert (bufDay);
+    // let dateOnString = String(year)+""+String(mounth)+String(bufDay);
+    // let newDate = (dateOnString);
+    let newDate = new Date(year, mounth , bufDay);
+    alert (newDate);
+
+
+}
+
+let date = new Date(2020, 8, 12);
+getDateAgo(date, 2);

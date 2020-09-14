6b2c01712145391b66b43555412bba81f4166e29
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-14 20:36:32 +0300

    add exercise solution 'convert object to JSON and conversely'

--- JS-JSON/script1.js
+++ JS-JSON/script1.js
@@ -1,2 +1,11 @@
 'use strict'
 
+let user = {
+    name: "Василий Иванович",
+    age: 35
+  };
+
+  let JSONFormatUser = JSON.stringify(user);
+  alert(JSONFormatUser);
+  let JSONParseUSer = JSON.parse(JSONFormatUser);
+  alert(JSONParseUSer.name + " " + JSONParseUSer.age + " годиков");

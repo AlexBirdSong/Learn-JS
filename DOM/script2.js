338aa95769c613dd9643cf25ba1633129d80a61b
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-18 23:11:34 +0300

    add exercise solution 'Select cells diagonally'

--- DOM/script2.js
+++ DOM/script2.js
@@ -1,2 +1,10 @@
 'use strict'
 
+let table = document.body.firstElementChild;
+
+for(let i = 0; i <= 4; i++)
+    for(let j = 0; j <= 4; j++)
+    if (i==j){
+        table.rows[i].cells[j].style.backgroundColor = 'red' 
+    }
+

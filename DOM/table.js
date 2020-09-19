74fec575c4dbd32859e61e541b50458a966b218f
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-19 22:50:12 +0300

    add exercise solution 'Search in table'

--- DOM/table.js
+++ DOM/table.js
@@ -1,2 +1,17 @@
 'use strict'
 
+//alert(document.getElementById("age-table"));
+
+// let labels = window['age-table'].getElementsByTagName('label');
+// for (let label of labels){
+//     alert( label );
+// }
+
+// alert( window['age-table'].getElementsByTagName('td')[0] );
+
+// alert(document.getElementsByName("search"));
+
+// alert(document.querySelector('input'));
+
+// let inputs = document.getElementsByName('search')[0].querySelectorAll('input');
+// alert(inputs[inputs.length-1]);

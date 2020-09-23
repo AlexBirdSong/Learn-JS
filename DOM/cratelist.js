865d0435f38b270376dc2fe7452f4f15f36cc660
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-23 22:43:49 +0300

    Add exercise solution 'crate list'

--- DOM/cratelist.js
+++ DOM/cratelist.js
@@ -1,2 +1,17 @@
 'use strict'
 
+function createli(elem,listring){
+  let li = document.createElement('li');
+  elem.append(li);
+  li.append(listring);
+}
+let listring;
+let elem = document.getElementById("ul");
+do
+{
+  listring = prompt("Введите содержимое:");
+  createli(elem,listring);
+}
+while(listring != "")
+
+elem.lastChild.remove();

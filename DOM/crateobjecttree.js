2faaefedd11f21935346177977d267030e63f6c1
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-24 22:14:45 +0300

    add files for solving exercise 'Create object tree'

---
+++ DOM/crateobjecttree.js
@@ -0,0 +1,25 @@
+'use strict'
+
+
+
+
+
+let data = {
+  "Рыбы": {
+    "форель": {},
+    "лосось": {}
+  },
+
+  "Деревья": {
+    "Огромные": {
+      "секвойя": {},
+      "дуб": {}
+    },
+    "Цветковые": {
+      "яблоня": {},
+      "магнолия": {}
+    }
+  }
+};
+let container = document.getElementById('container');
+createTree(container, data);
